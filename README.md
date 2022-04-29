07235f84b3093e5c549e7542acfff9eb55b90666
Alister Burt <alisterburt@gmail.com>
2022-04-29 14:29:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Using napari with cryo-electron tomography data
+
+This repo contains a quick demonstration of how to use napari to visualise cryo-electron tomography data. 
+
+This demo was given after a presentation at the MRC Laboratory of Molecular Biology tomography meeting on 29/04/22. Slides can be downloaded from [zenodo](https://zenodo.org/record/6505039).
+
+# Installation
+Install into a fresh virtual environment, I personally use [miniconda](https://docs.conda.io/en/latest/miniconda.html) for environment management.
+
+```commandline
+git clone https://github.com/alisterburt/napari-cryo-et-demo.git
+pip install -r requirements.txt
+```
+
+# Downloading example data
+
+We will download [example data from zenodo](https://zenodo.org/record/6504891) using `zenodo_get` which was installed in the previous step.
+
+```commandline
+zenodo_get --output-dir hiv 6504891
+```
+
+# Running
+Run the demo script with 
+
+```commandline
+python visualise_particles_on_tomogram.py
+```
---
+++ visualise_particles_on_tomogram.py
@@ -0,0 +1,51 @@
+import mrcfile
+import napari
+import numpy as np
+import starfile
+from scipy.spatial.transform import Rotation as R
+
+# files containing data
+tomogram_file = 'hiv/01_10.00Apx.mrc'
+particles_file = 'hiv/01_10.00Apx_particles.star'
+
+# loading data into memory
+# tomogram is a numpy array containing image array data
+# df is a pandas DataFrame containing table of info from STAR file
+with mrcfile.open(tomogram_file) as mrc:
+    tomogram = mrc.data.copy()
+df = starfile.read(particles_file)
+
+# get particle positions as (n, 3) numpy array from DataFrame
+xyz = df[['rlnCoordinateX', 'rlnCoordinateY', 'rlnCoordinateZ']].to_numpy()
+
+# get particle orientations as Euler angles from DataFrame
+euler_angles = df[['rlnAngleRot', 'rlnAngleTilt', 'rlnAnglePsi']].to_numpy()
+
+# turn Euler angles into a scipy 'Rotation' object, rotate Z vectors to see
+# where they point for the aligned particle
+rotations = R.from_euler(seq='ZYZ', angles=euler_angles, degrees=True).inv()
+rotated_z_vectors = rotations.apply([0, 0, 1])
+
+
+# instantiate a napari viewer
+viewer = napari.Viewer(ndisplay=3)
+
+# set up napari vectors layer data
+# (n, 2, 3) array
+# dim 0: batch dimension
+# dim 1: first entry is start point of vector
+# dim 2: components of direction vector e.g. (x, y, z)
+vectors = np.zeros((rotated_z_vectors.shape[0], 2, 3))
+
+# set vectors data
+vectors[:, 0, :] = xyz[:, ::-1]
+vectors[:, 1, :] = rotated_z_vectors[:, ::-1]
+
+# add data to viewer
+viewer.add_image(tomogram, blending='translucent_no_depth', colormap='gray_r')
+viewer.add_points(xyz[:, ::-1], face_color='cornflowerblue', size=10)
+viewer.add_vectors(vectors, length=10, edge_color='orange')
+
+# this line is required for the GUI window to be kept open when running
+# napari from a script
+napari.run()
